d84c8d3371efc3ae57d98f652fa2ec6f617fb613
RGSntj <mariadefatima071965@gmail.com>
2025-03-22 16:00:34 -0300

    feat: styles chat screen

--- src/screens/chat/index.tsx
+++ src/screens/chat/index.tsx
@@ -1,6 +1,13 @@
-import { SafeAreaView, Text } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  TextInput,
+} from "react-native";
 
-import { RouteProp, useRoute } from "@react-navigation/native";
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 
 import { s } from "./styles";
 import { RootStackParamList } from "../../types/navigation";
@@ -9,10 +16,17 @@ import { api } from "../../services/api";
 import { getUserData } from "../../storages/userStorage";
 import { IRoomDetail } from "../../interfaces/roomDetails";
 
+import { Ionicons } from "@expo/vector-icons";
+import Foundation from "@expo/vector-icons/Foundation";
+
+import defaultProfile from "../../assets/profile-2.png";
+
 export function ChatScreen() {
   const [room, setRoom] = useState<IRoomDetail | null>(null);
 
   const { params } = useRoute<RouteProp<RootStackParamList>>();
+
+  const { goBack } = useNavigation();
 
   useEffect(() => {
     async function fetchRoomDetails() {
@@ -37,9 +51,52 @@ export function ChatScreen() {
 
   return (
     <SafeAreaView style={s.container}>
-      <Text>Chat screen</Text>
+      <View style={s.header}>
+        <View style={s.rightContent}>
+          <TouchableOpacity
+            style={s.button}
+            activeOpacity={0.7}
+            onPress={goBack}
+          >
+            <Ionicons name="arrow-back" size={20} />
+          </TouchableOpacity>
 
-      <Text>{room?.participant.username}</Text>
+          <View style={s.profilePhoto}>
+            <Image
+              source={defaultProfile}
+              resizeMode="contain"
+              style={{ maxHeight: 40, maxWidth: 40 }}
+            />
+          </View>
+
+          <View style={s.userInformations}>
+            <Text style={s.username}>{room?.participant.username}</Text>
+            <Text style={s.userStatus}>online</Text>
+          </View>
+        </View>
+
+        <View style={s.leftContent}>
+          <TouchableOpacity style={s.button} activeOpacity={0.7}>
+            <Foundation name="telephone" size={20} color="#222" />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={s.containerMessages}></View>
+
+      <View style={s.sendMessagesArea}>
+        <View style={s.containerInput}>
+          <TouchableOpacity style={s.plusButton} activeOpacity={0.7}>
+            <Ionicons name="add" size={20} color="#ffffff" />
+          </TouchableOpacity>
+
+          <TextInput style={s.input} placeholder="Digite sua mensagem..." />
+        </View>
+
+        <TouchableOpacity style={s.sendMessageButton} activeOpacity={0.7}>
+          <Ionicons name="send-outline" size={18} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
--- src/screens/chat/styles.ts
+++ src/screens/chat/styles.ts
@@ -1,9 +1,106 @@
 import { StyleSheet } from "react-native";
 import { COLORS } from "../../utils/colors";
+import { FONTS } from "../../utils/fonts";
 
 export const s = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.background,
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    // backgroundColor: "#f1f1f1",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+
+  rightContent: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+
+  username: {
+    fontFamily: FONTS.bold,
+    fontSize: 15,
+  },
+
+  userStatus: {
+    fontFamily: FONTS.regular,
+    fontSize: 12,
+    color: "#affc41",
+  },
+
+  profilePhoto: {
+    maxHeight: 45,
+    maxWidth: 45,
+    backgroundColor: "#3B3561",
+    borderRadius: 60 / 2,
+  },
+
+  userInformations: {
+    display: "flex",
+  },
+
+  button: {
+    height: 40,
+    width: 40,
+    // backgroundColor: "#f5f5f5",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+
+    borderRadius: 60 / 2,
+  },
+
+  leftContent: {},
+
+  containerMessages: {
+    flex: 1,
+    backgroundColor: "#f1f1f1",
+  },
+
+  sendMessagesArea: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    // paddingVertical: 20,
+    height: 90,
+    paddingHorizontal: 20,
+    gap: 10,
+  },
+
+  containerInput: {
+    backgroundColor: "#f2f2f2",
+    borderRadius: 30,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    height: 50,
+    paddingHorizontal: 10,
+    flex: 1,
+  },
+
+  plusButton: {
+    backgroundColor: "#9d4edd",
+    borderRadius: 30,
+    padding: 5,
+  },
+
+  input: {
+    flex: 1,
+    fontFamily: FONTS.regular,
+  },
+
+  sendMessageButton: {
+    // backgroundColor: "#f2f2f2",
+    padding: 10,
+    // borderRadius: 60 / 2,
   },
 });
